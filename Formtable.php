d15a9b205014fc2b711a66da152485e622f98187
emengstu <35929013+emengstu@users.noreply.github.com>
2018-03-05 11:29:57 -0500

    Update Formtable.php

--- Formtable.php
+++ Formtable.php
@@ -1,4 +1,15 @@
 <html>
 <head>
   <title> Form Table </title>
+  <?php
+      include 'bslinks.php';
+  ?>
+  <link rel="stylesheet" href="css/main=php.css">
+  </head>
+  <body>
+        <div class="content">
+        <div class="container">
+            <div class="row">
+                  <h1> Enter Data <h1><br>
+                 
   
