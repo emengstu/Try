393f37d9fd114c4b766e8709cbd3eddc164ca751
emengstu <35929013+emengstu@users.noreply.github.com>
2018-03-07 11:41:00 -0500

    Update Formtable.php

--- Formtable.php
+++ Formtable.php
@@ -3,6 +3,10 @@
   <title> Form Table </title>
   <?php
       include 'bslinks.php';
+	$a= "select * from authors order by last_name, first_name";
+	require_once 'liblogin.php';
+	$conn = new mysqli($hostname, $user, $pword, $database, 3305, '/Applications/MAMP/tmp/mysql/mysql.sock');
+	if ($conn->connect_error)die($conn->connect_error);
   ?>
   <link rel="stylesheet" href="css/main=php.css">
   </head>
